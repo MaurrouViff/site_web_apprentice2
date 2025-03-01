5ba8487ee85859eef3d6ae486c35d225d21fd9af
MaurrouViff <rofldu08@gmail.com>
2025-03-01 17:56:20 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gothic: ['Blackletter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
